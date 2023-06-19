64933adee1e83f3218e00850b135476ace2a145c
Jayasurya-Marasani <jayasuryamarasani@gmail.com>
2023-06-19 19:41:34 +0530

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+cp -R dotconfig/* /home/$username/.config/
+cp bg.jpg /home/$username/Pictures/
+mv user-dirs.dirs /home/$username/.config
+mv .xsession /home/$username/
+mv .xinitrc /home/$username/
+chown -R $username:$username /home/$username
+
+# Installing Essential Programs 
+nala install feh alacritty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol polybar build-essential libx11-dev libxft-dev libxinerama-dev bspwm sxhkd -y
+# Installing Other less important Programs
+nala install neofetch flameshot psmisc mangohud neovim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd $builddir 
+nala install fonts-font-awesome
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+# Install brave-browser
+nala install apt-transport-https curl -y
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+nala update
+nala install brave-browser -y
+
+# Enable graphical login and change target from CLI to GUI
+systemctl enable lightdm
+systemctl set-default graphical.target
+
+# Beautiful bash
+git clone https://github.com/ChrisTitusTech/mybash
+cd mybash
+bash setup.sh
+cd $builddir
+
+# Use nala
+bash scripts/usenala
